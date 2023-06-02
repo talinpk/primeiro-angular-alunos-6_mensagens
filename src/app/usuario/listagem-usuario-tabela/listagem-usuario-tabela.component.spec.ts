c364a430f66837b61425903807fe377c2fadb9da
talinpk <96902115+talinpk@users.noreply.github.com>
2023-06-02 00:11:05 -0300

    mensagem do commit

---
+++ src/app/usuario/listagem-usuario-tabela/listagem-usuario-tabela.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListagemUsuarioTabelaComponent } from './listagem-usuario-tabela.component';
+
+describe('ListagemUsuarioTabelaComponent', () => {
+  let component: ListagemUsuarioTabelaComponent;
+  let fixture: ComponentFixture<ListagemUsuarioTabelaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListagemUsuarioTabelaComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ListagemUsuarioTabelaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/usuario/listagem-usuario-tabela/listagem-usuario-tabela.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+
+import { Usuario } from 'src/app/shared/modelo/usuario';
+import {UsuarioService} from '../../shared/services/usuario.service';
+import { MensagemService } from 'src/app/shared/services/mensagem.service';
+
+
+@Component({
+  selector: 'app-listagem-tabela',
+  templateUrl: './listagem-tabela.component.html',
+  styleUrls: ['./listagem-tabela.component.css'],
+})
+export class ListagemTabelaComponent {
+
+  colunas:string[] = ['id','nome','idade','cpf','acoes']
+  usuarios: Usuario[] = [];
+  tabelaDeUsuarios = new MatTableDataSource(this.usuarios);
+
+  constructor(private usuarioService: UsuarioService, private messagemService: MensagemService) {
+  }
+
+  ngOnInit(): void {
+    this.usuarioService.listar().subscribe(
+      usuariosRetornados =>{
+        this.usuarios = usuariosRetornados;
+        this.tabelaDeUsuarios = new MatTableDataSource(this.usuarios)}
+    );
+  }
+
+  aplicarFiltro(evento: Event) {
+    const valorFiltro = (evento.target as HTMLInputElement).value;
+    this.tabelaDeUsuarios.filter = valorFiltro.trim().toLowerCase();
+  }
+
+  excluir(usuarioARemover: Usuario): void {
+    if (usuarioARemover.id) {
+      this.usuarioService.apagar(usuarioARemover.id).subscribe(
+        usuarioRemovido => {
+          const indx = this.usuarios.findIndex(usuario =>
+            usuario.id === usuarioARemover.id);
+          this.usuarios.splice(indx, 1);
+          this.messagemService.sucesso("Usuário excluido com Sucesso.")
+          this.tabelaDeUsuarios = new MatTableDataSource(this.usuarios)
+
+        }
+      );
+    }
+  }
+}
